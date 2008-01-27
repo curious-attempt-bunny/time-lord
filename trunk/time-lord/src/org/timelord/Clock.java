d0e417d09cbe12e22737cede58e909d1b61a4f23
curious.attempt.bunny <curious.attempt.bunny@42b76160-a844-0410-b38f-a3da3638b2b2>
2008-01-27 00:12:47 +0000

    added license headers to source files, added intercepts for the remaining Calendar.getInstance methods

--- trunk/time-lord/src/org/timelord/Clock.java
+++ trunk/time-lord/src/org/timelord/Clock.java
@@ -1,13 +1,31 @@
+/*
+ *  Copyright 2008 Merlyn Albery-Speyer 
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
 package org.timelord;
 
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public class Clock {
 
 	private static ThreadLocal<ClockThreadState> threadLocalState = new ThreadLocal<ClockThreadState>();
-	
+
 	public static void thaw() {
 		getClockState().setFrozen(false);
 		getClockState().setTimeNow(0L);
@@ -17,46 +35,50 @@ public class Clock {
 		getClockState().setFrozen(true);
 		getClockState().setTimeNow(System.currentTimeMillis());
 	}
-	
+
 	private static ClockThreadState getClockState() {
 		ClockThreadState threadState = threadLocalState.get();
-		
+
 		if (threadState == null) {
 			threadState = new ClockThreadState();
 			threadLocalState.set(threadState);
 		}
-		
+
 		return threadState;
 	}
 
 	public static Calendar getCalendar() {
-		Calendar result = Calendar.getInstance();
-		
-		if (getClockState().isFrozen()) {
-			result.setTimeInMillis(getClockState().getTimeNow());
-		}
-		
-		return result;
+		return getCorrectCalendar(Calendar.getInstance());
 	}
-	
-//	public static Calendar getCalendar(Locale locale) {
-//		Calendar result = Calendar.getInstance(locale);
-//		
-//		if (getClockState().isFrozen()) {
-//			result.setTimeInMillis(getClockState().getTimeNow());
-//		}
-//		
-//		return result;
-//	}
-	
+
+	public static Calendar getCalendar(Locale locale) {
+		return getCorrectCalendar(Calendar.getInstance(locale));
+	}
+
+	public static Calendar getCalendar(TimeZone timezone) {
+		return getCorrectCalendar(Calendar.getInstance(timezone));
+	}
+
+	public static Calendar getCalendar(TimeZone timezone, Locale locale) {
+		return getCorrectCalendar(Calendar.getInstance(timezone, locale));
+	}
+
+	private static Calendar getCorrectCalendar(Calendar calendar) {
+		if (getClockState().isFrozen()) {
+			calendar.setTimeInMillis(getClockState().getTimeNow());
+		}
+
+		return calendar;
+	}
+
 	public static Date getDate() {
 		if (getClockState().isFrozen()) {
 			return new Date(getClockState().getTimeNow());
 		}
-		
+
 		return new Date();
 	}
-	
+
 	public static long currentTimeMillis() {
 		if (getClockState().isFrozen()) {
 			return getClockState().getTimeNow();
@@ -69,22 +91,22 @@ public class Clock {
 
 		private long timeNow;
 		private boolean isFrozen;
-		
+
 		public void setTimeNow(long timeNow) {
 			this.timeNow = timeNow;
 		}
-		
+
 		public long getTimeNow() {
 			return timeNow;
 		}
-		
+
 		public void setFrozen(boolean isFrozen) {
 			this.isFrozen = isFrozen;
 		}
-		
+
 		public boolean isFrozen() {
 			return isFrozen;
 		}
-		
+
 	}
 }
--- trunk/time-lord/src/org/timelord/TimeLordClassLoader.java
+++ trunk/time-lord/src/org/timelord/TimeLordClassLoader.java
@@ -1,3 +1,20 @@
+/*
+ *  Copyright 2008 Merlyn Albery-Speyer 
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
 package org.timelord;
 
 import javassist.CannotCompileException;
@@ -6,7 +23,7 @@ import javassist.CodeConverter;
 import javassist.CtClass;
 
 public class TimeLordClassLoader extends java.lang.ClassLoader {
-	private ClassPool pool;
+	private final ClassPool pool;
 
 	public TimeLordClassLoader(ClassLoader classLoader) {
 		super(classLoader);
@@ -17,19 +34,23 @@ public class TimeLordClassLoader extends java.lang.ClassLoader {
 	@Override
 	public Class<?> loadClass(String name) throws ClassNotFoundException {
 		if (isInstrumentable(name)) {
-			return loadInstrumentedClass(name);
+			return loadAndInstrumentClass(name);
 		} else {
 			return super.loadClass(name);
 		}
 	}
 
-	private Class<?> loadInstrumentedClass(String name)
+	private boolean isInstrumentable(String name) {
+		return !name.startsWith("java.") && !name.startsWith("sun.");
+	}
+
+	private Class<?> loadAndInstrumentClass(String name)
 			throws ClassFormatError, ClassNotFoundException {
 		try {
 			CtClass cc = pool.get(name);
 
 			if (!cc.isInterface()) {
-				if (!name.equals("org.timelord.Clock")) {
+				if (!isTimelordClockImplementation(name)) {
 					instrumentClass(name, cc);
 				}
 			}
@@ -37,14 +58,18 @@ public class TimeLordClassLoader extends java.lang.ClassLoader {
 			byte[] b = cc.toBytecode();
 			return defineClass(name, b, 0, b.length);
 		} catch (Exception e) {
-			throw new ClassNotFoundException("", e);
+			throw new RuntimeException(
+					"Failed to load define instrumented class: " + name, e);
 		}
+	}
+
+	private boolean isTimelordClockImplementation(String name) {
+		return name.equals("org.timelord.Clock");
 	}
 
 	private void instrumentClass(String name, CtClass cc)
 			throws CannotCompileException {
-		System.out.println("Instrumenting " + name);
-
+		// TODO cache this CodeCoverter
 		CodeConverter codeConverter = new CodeConverter();
 		try {
 			CtClass clock = pool.get("org.timelord.Clock");
@@ -59,16 +84,31 @@ public class TimeLordClassLoader extends java.lang.ClassLoader {
 					.getDeclaredMethod("getInstance"), clock
 					.getDeclaredMethod("getCalendar"));
 
+			CtClass locale = pool.get("java.util.Locale");
+			codeConverter
+					.redirectMethodCall(calendar.getDeclaredMethod(
+							"getInstance", new CtClass[] { locale }), clock
+							.getDeclaredMethod("getCalendar",
+									new CtClass[] { locale }));
+
+			CtClass timezone = pool.get("java.util.TimeZone");
+			codeConverter.redirectMethodCall(calendar.getDeclaredMethod(
+					"getInstance", new CtClass[] { timezone }), clock
+					.getDeclaredMethod("getCalendar",
+							new CtClass[] { timezone }));
+
+			codeConverter.redirectMethodCall(calendar.getDeclaredMethod(
+					"getInstance", new CtClass[] { timezone, locale }), clock
+					.getDeclaredMethod("getCalendar", new CtClass[] { timezone,
+							locale }));
+
 			CtClass date = pool.get("java.util.Date");
 			codeConverter.replaceNew(date, clock, "getDate");
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new RuntimeException("Failed to create CodeCoverter", e);
 		}
 
 		cc.instrument(codeConverter);
 	}
 
-	private boolean isInstrumentable(String name) {
-		return !name.startsWith("java.") && !name.startsWith("sun.");
-	}
 }
