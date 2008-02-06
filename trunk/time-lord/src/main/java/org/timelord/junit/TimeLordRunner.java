281cb2abf788d28dcf1e2cdaa08d1326ef3b33d4
curious.attempt.bunny <curious.attempt.bunny@42b76160-a844-0410-b38f-a3da3638b2b2>
2008-02-06 05:44:06 +0000

    added a JUnit 4 Runner for use with @RunWith: TimeLordRunner

---
+++ trunk/time-lord/src/main/java/org/timelord/junit/TimeLordRunner.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2008 Merlyn Albery-Speyer 
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+package org.timelord.junit;
+
+import org.junit.internal.runners.InitializationError;
+import org.junit.internal.runners.TestClassRunner;
+import org.junit.runner.notification.RunNotifier;
+import org.timelord.Clock;
+
+public class TimeLordRunner extends TestClassRunner {
+
+	public TimeLordRunner(Class<?> test) throws InitializationError {
+		super(test);
+	}
+
+	@Override
+	public void run(RunNotifier notifier) {
+		try {
+			Clock.freeze();
+			super.run(notifier);
+		} finally {
+			Clock.thaw();
+		}
+	}
+}
